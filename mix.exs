6b6b5d2467e910f893e1208dcb6a366f49316523
Josh Austin <josh.austin@gmail.com>
2022-08-09 14:34:13 -0400

    update deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule RacingTelemetry.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:swarm, "~> 3.4"},
     ]
   end
 
